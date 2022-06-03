f9751edd609287e5b143590b782932ae914043b2
greinisch <greinisch@youwol.com>
2022-06-03 08:55:38 +0200

    🔥 remove unused views/models

--- src/lib/asset-card/models.ts
+++
@@ -1,5 +0,0 @@
-import { AssetsBackend } from '@youwol/http-clients'
-
-export interface AssetWithPermissions extends AssetsBackend.GetAssetResponse {
-    permissions: AssetsBackend.GetPermissionsResponse
-}
